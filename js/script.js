c5f473ef54bd180a14315b94988f8df89b8d892b
kaikerp <kaikerp@gmail.com>
2021-12-20 11:34:23 -0300

    js finalizado

--- js/script.js
+++ js/script.js
@@ -1,51 +1,69 @@
-// function livro(nome, ano, autor) {
-//   const contaAnos = new Date().getFullYear() - ano;
-//   const frase = nome.toUpperCase() + ", por " + autor + ", de " + ano + ".";
-//   const objeto = {
-//     contaAnos,
-//     frase,
-//   };
-//   return objeto;
-// }
-// const Livro = livro("O Senhor dos Anéis", 1954, "J.R.R. Tolkien");
-// console.log(Livro.frase);
-// console.log(Livro);
-// function livro(nome, ano, autor) {
-//   return {
-//     contaAnos: new Date().getFullYear() - ano,
-//     frase: nome.toUpperCase() + ", por " + autor + ", de " + ano + ".",
-//   };
-// }
+const links = document.querySelectorAll(".header-menu a");
 
-// const Livro = livro("O Senhor dos Anéis", 1954, "J.R.R. Tolkien");
-// console.log(Livro.frase);
-// console.log(Livro);
+function ativarLinks(link) {
+  const url = location.href;
+  const href = link.href;
 
-// const curso = {
-//   nome: "JavaScript",
-//   completar() {
-//     console.log(this.nome + ": Curso Completo.");
-//   },
-//   horas: "40 horas",
-// };
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLinks);
 
-// curso.completar();
-// console.log(curso);
+//---------------------
 
-const circulo = document.querySelector("#div");
+const parametros = new URLSearchParams(location.search);
 
-function seguirMouse(event) {
-  circulo.style.top = event.y + "px";
-  circulo.style.left = event.x + "px";
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
 }
 
-window.addEventListener("mousemove", seguirMouse);
-// ------
-const botao = document.querySelector(".botao");
+parametros.forEach(ativarProduto);
 
-function mostrar() {
-  const texto = document.querySelector(".texto");
-  texto.classList.toggle("ativo");
+//---------------------
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
 }
 
-botao.addEventListener("click", mostrar);
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//---------------------
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//-----------------------
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
